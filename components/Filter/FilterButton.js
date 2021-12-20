3f712d42b9108bc7281cbe79a8bdcb2979c47058
Eddie <elremolino91@gmail.com>
2021-12-20 13:42:06 -0800

    added fontawesome to dependencies, caret-down icon added to filterbutton component

--- components/Filter/FilterButton.js
+++ components/Filter/FilterButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
 
 const FilterButton = () => {
   return (
     <div className="w-[168px] bg-edvora-greyCard mt-11 text-edvora-greyFont">
       <button className="flex justify-between w-full h-[37.5px]  ">
         <span className="bg-rose-500 h-full ">product</span>
-        <span className="bg-green-500 h-full">Icon</span>
+        <span className="bg-green-500 h-full"><FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon></span>
       </button>
     </div>
   )
