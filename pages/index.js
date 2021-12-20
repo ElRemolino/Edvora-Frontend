82dc9e1d406d5f312c1fdb457770b9fd428c6e9e
Eddie <elremolino91@gmail.com>
2021-12-20 12:24:39 -0800

    made sidebar container and main content container

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="flex bg-rose-50 min-h-screen">
-      Main
-      <div className="bg-blue-200 md:bg-green-500  lg:bg-blue-500 w-[20%]">
-        sidebar
+    <div className="flex bg-edvora-greyCard min-h-screen">
+      <div className="bg-blue-200 md:bg-green-500  lg:bg-blue-500 w-[18%] ml-10 ">
+        <div className=" bg-edvora-black  ">
+          filterbox
+        </div>
       </div>
-      <div className=" bg-grey-200 md:bg-purple-500  lg:bg-orange-500 w-[70%] ">
+      <div className=" bg-grey-200 md:bg-purple-500  lg:bg-orange-500 w-[78%] ml-10 mr-10">
         main content
       </div>
     </div>
