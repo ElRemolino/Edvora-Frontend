d75a1c9738a147add478ae15fdc175431e1f621b
Eddie <elremolino91@gmail.com>
2021-12-20 14:01:26 -0800

    filter button spacing and icon added

--- components/Filter/FilterButton.js
+++ components/Filter/FilterButton.js
@@ -4,10 +4,10 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
 
 const FilterButton = () => {
   return (
-    <div className="w-[168px] bg-edvora-greyCard mt-11 text-edvora-greyFont">
-      <button className="flex justify-between w-full h-[37.5px]  ">
-        <span className="bg-rose-500 h-full ">product</span>
-        <span className="bg-green-500 h-full"><FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon></span>
+    <div className="w-[168px] bg-edvora-greyCard mt-11 text-edvora-greyFont ml-7 ">
+      <button className="flex justify-between w-full h-[37.5px] font-light text-[22px] ">
+        <span className="bg-rose-500 h-full  pl-2">product</span>
+          <FontAwesomeIcon className="mr-3 mt-2" icon={faCaretDown} size="lg"></FontAwesomeIcon>
       </button>
     </div>
   )
