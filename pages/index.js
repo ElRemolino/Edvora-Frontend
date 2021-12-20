78fa45088eafc06cbcb0a498ca05261219185255
Eddie <elremolino91@gmail.com>
2021-12-20 12:30:42 -0800

    filter rectangle container built

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="flex bg-edvora-greyCard min-h-screen">
-      <div className="bg-blue-200 md:bg-green-500  lg:bg-blue-500 w-[18%] ml-10 ">
-        <div className=" bg-edvora-black  ">
+    <div className="flex bg-edvora-greyCard min-h-screen min-w-screen">
+      <div className="bg-blue-200 md:bg-green-500  lg:bg-blue-500 ml-10 ">
+        <div className=" bg-edvora-black h-[275px] w-[228px] mx-10 mt-11 ">
           filterbox
         </div>
       </div>
