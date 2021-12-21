721eefa0edafb2958d7af626bc40244d188db896
Eddie <elremolino91@gmail.com>
2021-12-20 23:45:37 -0800

    added colors to tailwind theme

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import FilterContainer from '../components/Filter/FilterContainer';
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [brands, setBrands] = useState([])
 
   const getData = () => {
     return fetch('https://assessment-edvora.herokuapp.com')
@@ -17,13 +18,14 @@ export default function Home() {
           setData(items)
         }
       })
+
     return () => mounted = false;
   }, [])
 
   return (
     <div className="flex bg-edvora-greyCard min-h-screen min-w-screen">
       <div className="bg-blue-200 md:bg-green-500  lg:bg-blue-500 ml-10 ">
-        <FilterContainer />
+        <FilterContainer data={data} />
       </div>
       <div className=" bg-grey-200 md:bg-purple-500  lg:bg-orange-500 w-[78%] ml-10 mr-10">
         main content
