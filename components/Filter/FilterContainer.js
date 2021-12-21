319fbe88e9eb7ebb1132fa277b3fac069e3b52f1
Eddie <elremolino91@gmail.com>
2021-12-21 12:16:33 -0800

    changed Filter containers text to 22px

--- components/Filter/FilterContainer.js
+++ components/Filter/FilterContainer.js
@@ -4,7 +4,7 @@ import DropDownBtnWithCard from './DropDownBtnWithCard';
 const FilterContainer = ( { data } ) => {
   return (
     <div className=" bg-edvora-black h-[275px] w-[228px] mx-10 mt-11 rounded-2xl ">
-      <p className=" pt-6 pl-7  top-[8.72%] left-[12.2%] right-[91.22%] bottom-[88.15%] text-[20px] text-edvora-greyFont leading-6 ">Filters
+      <p className=" pt-6 pl-7  top-[8.72%] left-[12.2%] right-[91.22%] bottom-[88.15%] text-[22px] text-edvora-greyFont leading-6 ">Filters
       </p>
       <hr className="relative top-3 left-6 text-edvora-hr w-[160px]"></hr>
       <div className="space-y-4" >
