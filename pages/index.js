c98923432848784665321099ec56ade9ef49cafe
Eddie <elremolino91@gmail.com>
2021-12-21 22:13:06 -0800

    divs have md and mobile responsive features now. Carosel containers feel and look good

--- pages/index.js
+++ pages/index.js
@@ -30,40 +30,20 @@ export default function Home() {
 
   return (
     <div className="flex bg-edvora-greyCard min-h-screen min-w-screen">
-      <div className="bg-red-500 md:bg-green-500  lg:bg-blue-500 ml-10 ">
+      <div className="bg-red-500 md:bg-green-500  lg:bg-blue-500    ">
         <FilterContainer data={data} products={products} states={states} cities={cities} />
       </div>
 
       {/* Main Content Containers */}
-      <div className=" bg-purple-500">
-
-        {/* Headers Containers */}
-        <div className="relative pt-11 pb-4 lg:bg-red-200 ">
-          <h1 className=" text-white font-bold text-4xl ">Edvora</h1>
-          <h2 className=" text-edvora-greyFont h-[30px] pt-4 w-[97px] text-2xl font-medium ">Products</h2>
+      <div className=" bg-purple-500 p-10 md:w-[90%] w-[80vw]">
+        grid goes here
+        <div className=" bg-blue-400 ">
+          <h1 className=" bg-red-400 text-white font-bold text-4xl max-h-12 w-[150px] ">Edvora</h1>
+          <h2 className=" bg-red-400 text-edvora-greyFont relative top- h-15 w-[97px] text-2xl font-medium ">Products</h2>
         </div>
-
-        {/* Product Carosel Containers */}
-        <div className=" h-[90vh] min-w-[75vw] bg-yellow-500 pt-8 ">
-          <div className=" min-h-[50%] bg-green-500 border-4 border-white">
-            <h3 className="font-normal text-xl ">
-              Product Name
-            </h3>
-            <hr className=" bg-edvora-vector104 relative top-3 "/>
-            <div className="bg-black ">
-              rectangle container
-            </div>
-          </div>
-
-          <div className="min-h-[50%]  bg-green-500 border-4 border-white">
-            <h3 className="font-normal text-xl ">
-              Product Name
-            </h3>
-            <hr className=" bg-edvora-vector104 relative top-3 "/>
-            <div className="bg-black ">
-              rectangle container
-            </div>
-          </div>
+        <div className="bg-yellow-500 h-[80%] grid md:grid-rows-2 md:w-[]80vw">
+          <div className=" bg-green-400  mt-8 border-2 min-h-[30%] md:w-[65vw] md:m-10">carosel</div>
+          <div className=" bg-green-400  mt-8 border-2 min-h-[30%] md:w-[65vw] md:m-10">carosel</div>
         </div>
       </div>
     </div>
