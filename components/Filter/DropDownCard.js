7bfa1dd2af9ba4102566d4944c2e132c72885f04
Eddie <elremolino91@gmail.com>
2021-12-21 18:32:05 -0800

    li styling inside liCls variable

--- components/Filter/DropDownCard.js
+++ components/Filter/DropDownCard.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 const DropDownCard = ( { data, handleFilters, name } ) => {
   const [filters, setFilters] = useState({});
-  const liCls = "text-edvora-offwhiteFont hover:text-edvora-black hover:bg-edvora-hr font-medium font-bold h-full";
+  const liCls = "relative bottom-10 left-5 h-[40px] text-edvora-offwhiteFont bg-edvora-greyCard hover:text-edvora-black hover:bg-edvora-hr font-medium w-[160px]";
 
   return (
     <div className="relative flex-col columns-1 float-right bg-green-500">
       <ol className="absolute flex-col min-w-[160px]">
         {data.map((item, i) => (
-          <li  key={i} className="relative h-[40px] text-edvora-offwhiteFont bg-edvora-greyCard hover:text-edvora-black hover:bg-edvora-hr font-medium w-[160px] bottom-10 left-5" onClick={handleFilters} >
+          <li key={i} className={liCls} onClick={handleFilters} >
             {item}
           </li>
         ))}
