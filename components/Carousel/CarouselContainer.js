8dcecd1698f0036588e0c3f9838086c49928f04e
Eddie <elremolino91@gmail.com>
2021-12-23 14:37:16 -0800

    final commit

--- components/Carousel/CarouselContainer.js
+++ components/Carousel/CarouselContainer.js
@@ -6,30 +6,32 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 const CarouselContainer = ({ productFilter, filteredData }) => {
   const [counter, setCounter] = useState(4);
   let displayed = filteredData.slice(0, counter);
-  const [display, setDisplay] = useState([]);
+  const [display, setDisplay] = useState(displayed);
   let length = filteredData.length;
+  let initialCards = filteredData.slice(0, 4)
 
-  // const handleClick = () => {
-  //   console.log(length, counter);
-  //   console.log(display);
-  //   if (length - counter < 4) {
-  //     let newDisplay = filteredData.slice(counter);
-  //     setCounter(0);
-  //     setDisplay(newDisplay);
-  //   } else {
-  //     let newDisplay = filteredData.slice(counter, counter + 4);
-  //     setCounter(counter + 4);
-  //     setDisplay(newDisplay);
-  //   }
-  // }
+  const handleClick = () => {
+    console.log(length, counter);
+    console.log(display);
+    if (length - counter < 4) {
+      let newDisplay = filteredData.slice(counter);
+      setCounter(0);
+      setDisplay(newDisplay);
+    } else {
+      let newDisplay = filteredData.slice(counter, counter + 4);
+      setCounter(counter + 4);
+      setDisplay(newDisplay);
+    }
+  }
 
 
   return (
     <div className="lg:relative lg:top-14 lg:max-w-[70vw] lg:w-[80vw]">
       <h2 className="text-white text-2xl pb-4">{productFilter}</h2>
-      <hr className="text-edvora-hr lg:h-[10px] lg:w-[85%] "></hr>
+      <hr className="text-edvora-hr lg:h-[10px] lg:max-w-[940px] md:max-w-[80vw]"></hr>
       <div className="lg:flex lg:pt-4">
         <div className="bg-black grid lg:grid-cols-4 rounded-2xl lg:h-[80%] lg:max-w-[80vw] lg:p-5 lg:relative lg:right-3 lg:space-x-5  md:grid-cols-2 md:pl-5 md:pt-5 md:pb-5">
+
           {
               display.map((item, i) => {
                 return (
