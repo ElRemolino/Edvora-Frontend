9af594e0d228c12cd7ed346849bd75dba68567be
Eddie <elremolino91@gmail.com>
2021-12-23 15:58:03 -0800

    finished the carousel handleClick function. Made carousel a dumb componenet and passed its state to the index.js file

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export default function Home() {
   const [productFilter, setProductFilter] = useState('');
   const [stateFilter, setStateFilter] = useState('');
   const [cityFilter, setCityFilter] = useState('');
+  const [display, setDisplay] = useState([]);
+  const [counter, setCounter] = useState(4);
 
   useEffect(() => {
     fetch('https://assessment-edvora.herokuapp.com')
@@ -22,7 +24,7 @@ export default function Home() {
         const products = new Set();
         const states = new Set();
         const cities = new Set();
-
+        const initialDisplay = data.slice(0, 4)
         data.forEach(item => {
           products.add(item.brand_name);
           states.add(item.address.state);
@@ -33,6 +35,7 @@ export default function Home() {
         setProducts([...products]);
         setStates([...states]);
         setCities([...cities]);
+        setDisplay(initialDisplay);
       });
   }, []);
 
@@ -51,6 +54,7 @@ export default function Home() {
     }
   }, [stateFilter]);
 
+  //handle if user picks a new city
   useEffect(() => {
     if(cityFilter === "") {
       applyStateFilter();
@@ -60,7 +64,7 @@ export default function Home() {
     }
   }, [cityFilter])
 
-  //handle when user picks a city
+
   const applyCityFilter = () => {
     let filteredCities = [];
     let citiesAvailable = new Set();
@@ -113,21 +117,33 @@ export default function Home() {
     }
   }
 
+  const handleClick = () => {
+    console.log(filteredData.length, counter);
+    if (filteredData.length - counter < 4) {
+      let newDisplay = filteredData.slice(counter);
+      setCounter(0);
+      setDisplay(newDisplay);
+    } else {
+      let newDisplay = filteredData.slice(counter, counter + 4);
+      setCounter(counter + 4);
+      setDisplay(newDisplay);
+    }
+  }
+
   return (
     <div className="flex bg-edvora-greyCard min-h-screen min-w-screen">
       <div name="filter-container" className=" flex">
         <FilterContainer data={data} products={products} states={states} cities={cities} productFilter={productFilter} setProductFilter={setProductFilter} stateFilter={stateFilter} setStateFilter={setStateFilter} cityFilter={cityFilter} setCityFilter={setCityFilter} />
       </div>
-      {/* Main Content Containers */}
       <div className="lg:p-10 lg:relative lg:left-20 lg:w-[100vw]">
         <div className="top-8">
-          <h1 className=" text-white font-bold text-4xl max-h-12 w-[150px]">Edvora</h1>
-          <h2 className=" text-edvora-greyFont h-15 w-[97px] text-2xl font-medium relative top-8 ">Products</h2>
+          <h1 className="text-white font-bold text-4xl max-h-12 w-[150px]">Edvora</h1>
+          <h2 className="text-edvora-greyFont h-15 w-[97px] text-2xl font-medium relative top-8 ">Products</h2>
         </div>
-        <div className=" lg:min-h-[80vh] lg:w-[70vw] md:min-h-[80vh] md:w-[60vw]  ">
+        <div className="lg:min-h-[80vh] lg:w-[70vw] md:min-h-[80vh] md:w-[60vw]">
           <div className=" lg:min-h-[80%] lg:w-[70vw] lg:space-y-20 md:spaye-y-20">
-            <CarouselContainer productFilter={productFilter} filteredData={filteredData} />
-            <CarouselContainer productFilter={productFilter} filteredData={filteredData} />
+            <CarouselContainer handleClick={handleClick} display={display} productFilter={productFilter} filteredData={filteredData} />
+            <CarouselContainer handleClick={handleClick} display={display} productFilter={productFilter} filteredData={filteredData} />
           </div>
         </div>
       </div>
